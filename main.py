933b4f06953562cb6f8f6bd68f50b68780947aca
RoadRaller <azzzx.aps@gmail.com>
2026-06-04 14:44:52 +0000

    thu 4 jun

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import os
 
+#file func
+def update_file(info):
+    file = open("user.txt","a")
+    file.write(f'{info} \n')
+    file.close()
 
-def register(name,password)
+def get_info():
+    file = open("user.txt",'r')
+    info = file.readlines()
+    for l in info:
+        l = l[:-2]
+        print(l)
+    print(info)
 
+
+def register(name,password):
+    info = name+' '+password+' '+'0'
+    update_file(info)
+
+
+
+update_file('hello')
+get_file()
