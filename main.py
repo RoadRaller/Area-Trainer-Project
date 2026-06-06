8f63674a541b1ce6bf8eb21c3f5333b9002375ea
RoadRaller <azzzx.aps@gmail.com>
2026-06-06 13:53:40 +0000

    update file and prevent repeating ccounts, also fixed add file

--- main.py
+++ main.py
@@ -1,25 +1,47 @@
 import os
 
 #file func
-def update_file(info):
+def append_file(info):
     file = open("user.txt","a")
     file.write(f'{info} \n')
     file.close()
 
 def get_info():
     file = open("user.txt",'r')
-    info = file.readlines()
-    for l in info:
+    text = file.readlines()
+    info = []
+    for l in text:
         l = l[:-2]
-        print(l)
-    print(info)
+        l = l.split()
+        info.append(l)
+
+    return info
+
+def update_file(info):
+    file = open("user.txt",'w')
+    for u in info:
+        line = " ".join(line)
+        file.write(f'{line}\n') 
+
+            
+
+def search(user):
+    info = get_info()
+    for u in info:
+        if u[0] == user:
+            return u
+    return None
+
 
 
 def register(name,password):
     info = name+' '+password+' '+'0'
-    update_file(info)
+    if search(name) == None:
+        append_file(info)
+        print("register complete")
+    else:
+        print('name taken')
 
 
 
-update_file('hello')
-get_file()
+register("u1","1234")
