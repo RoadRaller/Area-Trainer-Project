756b9de70086f186967af4c18f24db22afba99f8
RoadRaller <azzzx.aps@gmail.com>
2026-06-10 15:07:16 +0000

    upd

--- main.py
+++ main.py
@@ -1,47 +1,64 @@
 import os
 
 #file func
-def append_file(info):
-    file = open("user.txt","a")
-    file.write(f'{info} \n')
-    file.close()
 
-def get_info():
+    
+
+def getInfo():
     file = open("user.txt",'r')
     text = file.readlines()
     info = []
     for l in text:
-        l = l[:-2]
+        l = l[:-1]
+        print(l)
         l = l.split()
+        print(l)
+        l[2] = int(l[2])
+        print(l)
         info.append(l)
-
+    print(info)
     return info
 
-def update_file(info):
+def pushFile(info):
     file = open("user.txt",'w')
     for u in info:
-        line = " ".join(line)
+        u[2] = str(u[2])
+        line = " ".join(u)
         file.write(f'{line}\n') 
+    file.close()
 
-            
 
-def search(user):
-    info = get_info()
+def addScore(user,new):
+    info = getInfo()
+    for u in info:
+        if u[0] == user:
+            u[2] += new
+    pushFile(info)
+
+def addInfo(new):
+    info = getInfo()
+    info.append(new)
+    pushFile(info)
+
+def findInfo(user):
+    info = getInfo()
     for u in info:
         if u[0] == user:
             return u
+        print(u)
     return None
 
 
 
 def register(name,password):
-    info = name+' '+password+' '+'0'
-    if search(name) == None:
-        append_file(info)
+    new = [name,password,0]
+    if findInfo(name) == None:
+        addInfo(new)
         print("register complete")
     else:
         print('name taken')
 
 
 
-register("u1","1234")
+
+getInfo()
