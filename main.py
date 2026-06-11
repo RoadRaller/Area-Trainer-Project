30554a6c631d8910b4831d5310c1b84412ebee62
RoadRaller <azzzx.aps@gmail.com>
2026-06-11 13:28:55 +0000

    made questions and score update only main menu and login to go

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import random
+
 
 #file func
 
@@ -9,12 +11,14 @@ def getInfo():
     text = file.readlines()
     info = []
     for l in text:
-        l = l[:-1]
-        print(l)
+        l = l.strip()
+
+        if not l:
+            continue
+        
         l = l.split()
-        print(l)
         l[2] = int(l[2])
-        print(l)
+        
         info.append(l)
     print(info)
     return info
@@ -33,6 +37,7 @@ def addScore(user,new):
     for u in info:
         if u[0] == user:
             u[2] += new
+    print(f"{new}score added, now{u[2]}")
     pushFile(info)
 
 def addInfo(new):
@@ -58,7 +63,65 @@ def register(name,password):
     else:
         print('name taken')
 
+def login():
+    pass
+#question
+
+def qRec():
+    w = random.randint(1,100)
+    h = random.randint(1,100)
+    return w,h,float(w*h)
+
+def qTri():
+    w = random.randint(1,100)
+    h = random.randint(1,100)
+    return w,h,float(w*h/2)
+
+def qCir():
+    r = random.randint(1,100)
+    return r,float(r*r)
+
+
+def genOpt(ans):
+    ind = random.randint(0,3)
+    opt = []
+    for i in range(4):
+        opt.append(float(random.randint(1,100)))
+    opt[ind] = ans
+    return opt,ind 
+
+def askQ(user,mode):
+    if   mode == "tri":
+        w,h,ans = qTri()
+        print("find area of triangle")
+        print(f"w:{w},h:{h}")
+    elif mode == "rec":
+        w,h,ans = qRec()
+        print("find area of rectangle")
+        print(f"w:{w},h:{h}")
+
+    elif mode == "cir":
+        r,ans = qCir()
+        print("find area of circle(pi)")
+        print(f"r:{r}")
+    
+    opt,ind = genOpt(ans)
+    for i in range(4):
+        print(f"option {i}:{opt[i]}")
+    inp = int(input("enter chosen option"))
+    if inp == ind:
+        addScore(user,2)
+    else:
+        inp = int(input("attempt 2:"))
+        if inp == ind:
+            addScore(user,1)
+        else:
+            print(f"answer:{opt[ind]}")
 
 
 
-getInfo()
+    
+
+
+askQ("u1","tri")
+findInfo("u1")
